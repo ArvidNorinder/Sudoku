f23b5fd7d1cb4145aa81c610185135a8ee89bf7f
Arvid Norinder <an223tm@student.lnu.se>
2021-03-04 16:12:49 +0100

    Fixad kolla row och col med boolean representationer

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,44 +1,39 @@
 public class Sudoku implements SudokuSolver {
     private int[][] sudoku;
+    //nio siffror som sedan har 9 positioner som kollar om en siffra finns på en row eller på en rad, representerat av
+    //true eller false
+    private boolean[][] booleanRow;
+    private boolean[][] booleanCol;
+
 
     public Sudoku(){
         sudoku = new int[9][9];
+        //index 0-8 osv
+        booleanRow = new boolean[9][9];
+        booleanCol = new boolean[9][9];
     }
 
     @Override
     public boolean isOutOfBounds(int r, int c) {
-        return !(0 <= r && r < 9 && 0 <= c && c < 9);
+        return false;
     }
 
     @Override
     public void setNumber(int r, int c, int nbr) {
-        if(isOutOfBounds(r, c)){
-            throw new IllegalArgumentException("r = " + r + ", c = " + c);
-        }else{
-            if(nbr < 1 || nbr > 9){
-                throw new IllegalArgumentException("nbr = " + nbr);
-            }else{
-                sudoku[r][c] = nbr;
-            }
-        }
+        //-1 för att siffran 1 motsvarar pos 0. Behandla row och col med -1 vid i
+        booleanRow[nbr - 1][r] = true;
+        booleanCol[nbr - 1][c] = true;
+        sudoku[r][c] = nbr;
     }
 
     @Override
     public int getNumber(int r, int c) {
-        if(isOutOfBounds(r, c)){
-            throw new IllegalArgumentException("r = " + r + ", c = " + c);
-        }else{
-            return sudoku[r][c];
-        }
+        return sudoku[r][c];
     }
 
     @Override
     public void clearNumber(int r, int c) {
-        if(isOutOfBounds(r, c)){
-            throw new IllegalArgumentException("r = " + r + ", c = " + c);
-        }else{
-            sudoku[r][c] = 0;
-        }
+        sudoku[r][c] = 0;
     }
 
     @Override
