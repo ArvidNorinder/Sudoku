cc7898267a7abacf040de98b3b55e6fd25f5b877
Arvid Norinder <an223tm@student.lnu.se>
2021-03-04 13:27:01 +0100

    Compleated easy methods

---
+++ src/Sudoku.java
@@ -0,0 +1,52 @@
+public class Sudoku implements SudokuSolver {
+    private int[][] sudoku;
+
+    public Sudoku(){
+        sudoku = new int[9][9];
+    }
+
+    @Override
+    public void setNumber(int r, int c, int nbr) {
+        sudoku[r][c] = nbr;
+    }
+
+    @Override
+    public int getNumber(int r, int c) {
+        return sudoku[r][c];
+    }
+
+    @Override
+    public void clearNumber(int r, int c) {
+        sudoku[r][c] = 0;
+    }
+
+    @Override
+    public boolean isValid(int r, int c, int nbr) {
+        return false;
+    }
+
+    @Override
+    public boolean isAllValid() {
+        return false;
+    }
+
+    @Override
+    public boolean solve() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        sudoku = new int[9][9];
+    }
+
+    @Override
+    public int[][] getMatrix() {
+        return sudoku;
+    }
+
+    @Override
+    public void setMatrix(int[][] nbrs) {
+        sudoku = nbrs;
+    }
+}
