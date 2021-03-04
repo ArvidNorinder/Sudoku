edc0726a878b4f5fa6f957154f271320e82005aa
Arvid Norinder <an223tm@student.lnu.se>
2021-03-04 16:10:17 +0100

    Fixad kolla row och col med boolean representationer

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,12 +1,25 @@
+import java.util.Arrays;
+
 public class Sudoku implements SudokuSolver {
     private int[][] sudoku;
+    //nio siffror som sedan har 9 positioner som kollar om en siffra finns på en row eller på en rad, representerat av
+    //true eller false
+    private boolean[][] booleanRow;
+    private boolean[][] booleanCol;
+
 
     public Sudoku(){
         sudoku = new int[9][9];
+        //index 0-8 osv
+        booleanRow = new boolean[9][9];
+        booleanCol = new boolean[9][9];
     }
 
     @Override
     public void setNumber(int r, int c, int nbr) {
+        //-1 för att siffran 1 motsvarar pos 0. Behandla row och col med -1 vid input
+        booleanRow[nbr - 1][r] = true;
+        booleanCol[nbr - 1][c] = true;
         sudoku[r][c] = nbr;
     }
 
